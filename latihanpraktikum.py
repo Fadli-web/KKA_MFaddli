2f2226967f5af349a1baa11617647a93275ee3ec
Fadli-web <mohammad_fadli_33rpl@student.smktelkom-mlg.sch.id>
2025-11-23 03:21:48 +0700

    Add files via upload

---
+++ latihanpraktikum.py
@@ -0,0 +1,24 @@
+nomor1 = 10
+nomor2 = 20
+nomor3 = 30
+
+tinggi1 = 160.5
+tinggi2 = 170.2
+tinggi3 = 180.1
+
+nama1 = "Andi"
+nama2 = "Budi"  
+nama3 = "Citra"
+
+buah1 = ["apel", "jeruk", "mangga"]
+buah2 = ["semangka", "salak", "nanas"]
+buah3 = ["anggur", "kiwi", "melon"]
+
+print(buah1, buah2, buah3)
+print(tinggi1, tinggi2, tinggi3)
+print(nama1, nama2, nama3)
+
+print(type(nomor1), type(nomor2), type(nomor3))
+print(type(tinggi1), type(tinggi2), type(tinggi3))
+print(type(nama1), type(nama2), type(nama3))
+print(type(buah1), type(buah2), type(buah3))
